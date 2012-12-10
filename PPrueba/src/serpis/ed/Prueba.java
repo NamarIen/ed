7b68e6af5de844eba29432da9115913ed86b13fb
NamarIen <apm.antimateria@gmail.com>
2012-12-10 12:09:09 +0100

    Añadido PPrueba

---
+++ PPrueba/src/serpis/ed/Prueba.java
@@ -0,0 +1,13 @@
+package serpis.ed;
+import java.util.Date;
+public class Prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola: "+new Date());
+	}
+
+}
